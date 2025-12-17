4499471c8cbd1cb8b1e9bb717c47b5236d20a735
nacime <nacime.laghezali3@etu.univ-lorraine.fr>
2025-12-17 15:13:04 +0100

    ajout de la classe tachedecoratuer

---
+++ SAE3_01/src/main/java/application/TacheDecorateur.java
@@ -0,0 +1,78 @@
+package application;
+
+import java.util.Date;
+
+public abstract class TacheDecorateur extends TacheAbstraite{
+
+    protected TacheAbstraite tacheDecoree;
+
+    public TacheDecorateur(TacheAbstraite tacheDecoree) {
+        super(tacheDecoree.getNom());
+        this.tacheDecoree = tacheDecoree;
+    }
+
+    @Override
+    public boolean verifierDependance() {
+        return tacheDecoree.verifierDependance();
+    }
+
+    @Override
+    public boolean ajouterDependance(TacheAbstraite t) {
+        return tacheDecoree.ajouterDependance(t);
+    }
+
+    @Override
+    public boolean supprimerDependance(TacheAbstraite t) {
+        return tacheDecoree.supprimerDependance(t);
+    }
+
+    @Override
+    public String afficher(String indient) {
+        return tacheDecoree.afficher(indient);
+    }
+
+
+    @Override
+    public int getId() { return tacheDecoree.getId(); }
+    @Override
+    public void setId(int id) { tacheDecoree.setId(id); }
+
+    @Override
+    public String getNom() { return tacheDecoree.getNom(); }
+    @Override
+    public void setNom(String nom) { tacheDecoree.setNom(nom); }
+
+    @Override
+    public String getDescription() { return tacheDecoree.getDescription(); }
+    @Override
+    public void setDescription(String description) { tacheDecoree.setDescription(description); }
+
+    @Override
+    public int getPriorite() { return tacheDecoree.getPriorite(); }
+    @Override
+    public void setPriorite(int priorite) { tacheDecoree.setPriorite(priorite); }
+
+    @Override
+    public Date getDateDebut() { return tacheDecoree.getDateDebut(); }
+    @Override
+    public void setDateDebut(Date dateDebut) { tacheDecoree.setDateDebut(dateDebut); }
+
+    @Override
+    public Date getDateFin() { return tacheDecoree.getDateFin(); }
+    @Override
+    public void setDateFin(Date dateFin) { tacheDecoree.setDateFin(dateFin); }
+
+    @Override
+    public int getDureeEstimee() { return tacheDecoree.getDureeEstimee(); }
+    @Override
+    public void setDureeEstimee(int dureeEstimee) { tacheDecoree.setDureeEstimee(dureeEstimee); }
+
+    @Override
+    public String getEtat() { return tacheDecoree.getEtat(); }
+    @Override
+    public void setEtat(String etat) { tacheDecoree.setEtat(etat); }
+
+    @Override
+    public String afficherDetails() { return tacheDecoree.afficherDetails();}
+}
+
