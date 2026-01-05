ef6f62040d8c24319cc2a4724af7b4c038c524d0
Enzo <enzo2.marchal@gmail.com>
2026-01-05 16:09:43 +0100

    Modicifation de la vueKanban pour les bouton

--- SAE3_01/src/main/java/application/vue/VueKanban.java
+++ SAE3_01/src/main/java/application/vue/VueKanban.java
@@ -16,7 +16,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
-public class VueKanban extends BorderPane implements Observateur {
+public class VueKanban extends BorderPane implements Observateur, VueProjet {
 
     private Projet projet;
     private ProjetService service;
@@ -45,9 +45,13 @@ public class VueKanban extends BorderPane implements Observateur {
         HBox header = new HBox(20);
         header.setAlignment(Pos.CENTER_LEFT);
         header.setPadding(new Insets(0, 0, 20, 0));
-        Button backButton = new Button("<-");
+
+        Button backButton = new Button("<- Dashboard");
+
+
         Label titreLabel = new Label(projet.getNom());
         titreLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+
         header.getChildren().addAll(backButton, titreLabel);
         this.setTop(header);
 
@@ -66,6 +70,9 @@ public class VueKanban extends BorderPane implements Observateur {
         addTacheBox.setPadding(new Insets(10));
 
         VBox deleteBox = new VBox(10);
+        Button btnListe = new Button("Vue Liste");
+        btnListe.setMaxWidth(Double.MAX_VALUE);
+        btnListe.setStyle("-fx-background-color: #59a7ff; -fx-border-color: #000000;");
 
         Label lblAdd = new Label("Ajouter Tache");
         tfTask = new TextField();
@@ -92,7 +99,7 @@ public class VueKanban extends BorderPane implements Observateur {
         ControleurSupprimerColonne ctrlSupprCol = new ControleurSupprimerColonne(projet, service, this);
         btnDeleteCol.setOnAction(ctrlSupprCol);
 
-        sidebar.getChildren().addAll(addTacheBox, btnDelete, btnDeleteCol);
+        sidebar.getChildren().addAll(addTacheBox, btnDelete, btnDeleteCol, btnListe);
         this.setRight(sidebar);
     }
 
