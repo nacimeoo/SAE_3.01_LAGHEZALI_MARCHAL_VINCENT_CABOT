d5f8263eadd92f209771644ec27590c7b4f07605
BuildTools <unconfigured@null.spigotmc.org>
2026-01-07 09:19:44 +0100

    Exclusion des taches archivés dans le chargement

--- SAE3_01/src/main/java/application/DAO/ITacheDAO.java
+++ SAE3_01/src/main/java/application/DAO/ITacheDAO.java
@@ -8,6 +8,7 @@ import java.util.List;
 public interface ITacheDAO {
     TacheAbstraite getTacheById(int id) throws Exception;
     List<TacheAbstraite> getAllTaches() throws Exception;
+    List<TacheAbstraite> getAllTachesArchivee() throws Exception;
     List<TacheAbstraite> getTachesByColonneId(int colonneId) throws Exception;
     void save(TacheAbstraite tache) throws Exception;
     void delete(int id) throws Exception;
