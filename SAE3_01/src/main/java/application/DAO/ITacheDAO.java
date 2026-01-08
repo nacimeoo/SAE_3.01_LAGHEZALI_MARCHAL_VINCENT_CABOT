3be622df0c46a0b9bf4d35ffae728cab756aa70f
nacime <nacime.laghezali3@etu.univ-lorraine.fr>
2026-01-08 08:51:45 +0100

    suppression de methode ou attribut inutile

--- SAE3_01/src/main/java/application/DAO/ITacheDAO.java
+++ SAE3_01/src/main/java/application/DAO/ITacheDAO.java
@@ -7,15 +7,12 @@ import java.util.List;
 
 public interface ITacheDAO {
     TacheAbstraite getTacheById(int id) throws Exception;
-    List<TacheAbstraite> getAllTaches() throws Exception;
     List<TacheAbstraite> getTachesByColonneId(int colonneId) throws Exception;
     void save(TacheAbstraite tache) throws Exception;
     void delete(int id) throws Exception;
     void update_detail(TacheAbstraite tache) throws Exception;
     void addDependanceDAO(int idTacheMere, int idSousTache) throws Exception;
-    void chargerSousTaches(TacheMere mere) throws Exception;
     void detacherSousTache(int idTache, int idColonne) throws Exception;
     void updateType(int id, int nouveauType) throws Exception;
-
     List<TacheAbstraite> getTachesArchivees(int idProjet) throws Exception;
 }
